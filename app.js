ab474e785dd680145b30645efb48ff236919821b
dabc4694 <darshan.shah130@gmail.com>
2017-07-19 18:28:00 -0500

    Added honeRoute function

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-/*The method response.write("String") does not work if we write after 
-response.end()
-
-Conclusion: Whatever written after response.end() will not be able to 
-do anything with the response object.
-*/
 var http = require('http');
 
 //create a server object:
 http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    //response.write('This is befor end.\n'); //write a response to the client
-    setInterval(function(){
-        response.write("This is before end\n");
-    }, 1000);
-    response.end("Hello World!\n"); //end the response
-    //response.write("This is after end");
+    homeRoute(request, response);
 }).listen(8080);
 console.log("App Running on 'localhost:8080'");
+
+function homeRoute(request, response){
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write('Header\n');
+    response.write('Search\n');
+    response.end("Footer");
+
+}
