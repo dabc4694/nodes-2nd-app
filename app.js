d062b0e6c47341fab8cc82900e5977fc02627e57
dabc4694 <darshan.shah130@gmail.com>
2017-07-19 17:34:28 -0500

    Check response.wirte does not work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 //create a server object:
 http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write('Hello World!'); //write a response to the client
-    response.end(); //end the response
+    response.write('This is befor end.\n'); //write a response to the client
+    response.end("Hello World!\n"); //end the response
+    response.write("This is after end");
 }).listen(8080);
