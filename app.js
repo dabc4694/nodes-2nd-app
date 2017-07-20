a0973e1c8a62397c1c2db39295b41e748d8028e4
dabc4694 <darshan.shah130@gmail.com>
2017-07-19 19:03:16 -0500

    Created the reouter.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+var router = require("./router.js");
 var http = require('http');
 
 //create a server object:
 http.createServer(function (request, response) {
-    homeRoute(request, response);
+    router.home(request, response);
+    router.user(request, response);
 }).listen(8080);
 console.log("App Running on 'localhost:8080'");
-
-function homeRoute(request, response){
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write('Header\n');
-    response.write('Search\n');
-    response.end("Footer");
-
-}
---
+++ router.js
@@ -0,0 +1,22 @@
+function home(request, response){
+    if(request.url === '/'){
+        
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write('Header\n');
+        response.write('Search\n');
+        response.end("Footer");
+    }
+}
+
+function user(request, response){
+    var username = request.url.replace("/","");
+    if(username.length > 0){
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write('Header\n');
+        response.write(username+'\n');
+        response.end("Footer");
+    }
+}
+
+module.exports.home = home;
+module.exports.user = user;
